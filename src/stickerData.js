31a3551dabc3827aac45065addac4e998c0eb9a5
NikitaSharma22 <nikitasharma2265@gmail.com>
2025-06-14 17:47:45 +0530

    Create stickerData.js

---
+++ src/stickerData.js
@@ -0,0 +1,25 @@
+// A list of all possible stickers kids can collect.
+
+export const allStickers = [
+    // Animals
+    { id: 's1', name: 'Lion', image: 'https://i.imgur.com/g30sPa3.png', category: 'Animals' },
+    { id: 's2', name: 'Panda', image: 'https://i.imgur.com/b3v5rJt.png', category: 'Animals' },
+    { id: 's3', name: 'Elephant', image: 'https://i.imgur.com/aTq2Iu8.png', category: 'Animals' },
+    { id: 's4', name: 'Monkey', image: 'https://i.imgur.com/CGoB66q.png', category: 'Animals' },
+    { id: 's5', name: 'Koala', image: 'https://i.imgur.com/KqYwR4f.png', category: 'Animals' },
+
+    // Space
+    { id: 's6', name: 'Rocket', image: 'https://i.imgur.com/qve1f8c.png', category: 'Space' },
+    { id: 's7', name: 'Earth', image: 'https://i.imgur.com/2A2n9aA.png', category: 'Space' },
+    { id: 's8', name: 'Saturn', image: 'https://i.imgur.com/z4Vf97s.png', category: 'Space' },
+    { id: 's9', name: 'Astronaut', image: 'https://i.imgur.com/mJgV57L.png', category: 'Space' },
+    
+    // Food
+    { id: 's10', name: 'Pizza', image: 'https://i.imgur.com/k2k5x23.png', category: 'Food' },
+    { id: 's11', name: 'Ice Cream', image: 'https://i.imgur.com/V9r1N8L.png', category: 'Food' },
+    { id: 's12', name: 'Apple', image: 'https://i.imgur.com/4R0m3vj.png', category: 'Food' },
+
+    // Mythology
+    { id: 's13', name: 'Ganesha', image: 'https://i.imgur.com/y1v2l7A.png', category: 'Mythology' },
+    { id: 's14', name: 'Hanuman', image: 'https://i.imgur.com/NLo4L0A.png', category: 'Mythology' },
+];
