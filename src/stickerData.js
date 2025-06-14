fc107c13f0275e2a4a001955de9c26eb6e578694
NikitaSharma22 <nikitasharma2265@gmail.com>
2025-06-14 17:57:24 +0530

    Update stickerData.js

--- src/stickerData.js
+++ src/stickerData.js
@@ -1,25 +1,23 @@
-// A list of all possible stickers kids can collect.
-
 export const allStickers = [
-    // Animals
-    { id: 's1', name: 'Lion', image: 'https://i.imgur.com/g30sPa3.png', category: 'Animals' },
-    { id: 's2', name: 'Panda', image: 'https://i.imgur.com/b3v5rJt.png', category: 'Animals' },
-    { id: 's3', name: 'Elephant', image: 'https://i.imgur.com/aTq2Iu8.png', category: 'Animals' },
-    { id: 's4', name: 'Monkey', image: 'https://i.imgur.com/CGoB66q.png', category: 'Animals' },
-    { id: 's5', name: 'Koala', image: 'https://i.imgur.com/KqYwR4f.png', category: 'Animals' },
+  // Animals
+  { id: 's1', name: 'Lion', image: 'https://cdn.freepik.com/uploads/2023/05/cute-cartoon-lion.png', category: 'Animals' },
+  { id: 's2', name: 'Panda', image: 'https://cdn.freepik.com/uploads/2022/08/cute-panda-sticker.png', category: 'Animals' },
+  { id: 's3', name: 'Elephant', image: 'https://cdn.freepik.com/uploads/2021/11/cute-elephant-cartoon.png', category: 'Animals' },
+  { id: 's4', name: 'Monkey', image: 'https://cdn.freepik.com/uploads/2020/12/cute-monkey-cartoon.png', category: 'Animals' },
+  { id: 's5', name: 'Koala', image: 'https://cdn.freepik.com/uploads/2021/09/cute-koala-cartoon.png', category: 'Animals' },
 
-    // Space
-    { id: 's6', name: 'Rocket', image: 'https://i.imgur.com/qve1f8c.png', category: 'Space' },
-    { id: 's7', name: 'Earth', image: 'https://i.imgur.com/2A2n9aA.png', category: 'Space' },
-    { id: 's8', name: 'Saturn', image: 'https://i.imgur.com/z4Vf97s.png', category: 'Space' },
-    { id: 's9', name: 'Astronaut', image: 'https://i.imgur.com/mJgV57L.png', category: 'Space' },
-    
-    // Food
-    { id: 's10', name: 'Pizza', image: 'https://i.imgur.com/k2k5x23.png', category: 'Food' },
-    { id: 's11', name: 'Ice Cream', image: 'https://i.imgur.com/V9r1N8L.png', category: 'Food' },
-    { id: 's12', name: 'Apple', image: 'https://i.imgur.com/4R0m3vj.png', category: 'Food' },
+  // Space
+  { id: 's6', name: 'Rocket', image: 'https://cdn.freepik.com/uploads/2022/02/cute-rocket-cartoon.png', category: 'Space' },
+  { id: 's7', name: 'Earth', image: 'https://cdn.freepik.com/uploads/2022/03/cute-earth-cartoon.png', category: 'Space' },
+  { id: 's8', name: 'Saturn', image: 'https://cdn.freepik.com/uploads/2021/10/cute-saturn-cartoon.png', category: 'Space' },
+  { id: 's9', name: 'Astronaut', image: 'https://cdn.freepik.com/uploads/2020/10/cute-astronaut-cartoon.png', category: 'Space' },
 
-    // Mythology
-    { id: 's13', name: 'Ganesha', image: 'https://i.imgur.com/y1v2l7A.png', category: 'Mythology' },
-    { id: 's14', name: 'Hanuman', image: 'https://i.imgur.com/NLo4L0A.png', category: 'Mythology' },
+  // Food
+  { id: 's10', name: 'Pizza', image: 'https://cdn.freepik.com/uploads/2022/05/cute-pizza-cartoon.png', category: 'Food' },
+  { id: 's11', name: 'Ice Cream', image: 'https://cdn.freepik.com/uploads/2021/06/cute-ice-cream-cartoon.png', category: 'Food' },
+  { id: 's12', name: 'Apple', image: 'https://cdn.freepik.com/uploads/2022/01/cute-apple-cartoon.png', category: 'Food' },
+
+  // Mythology
+  { id: 's13', name: 'Ganesha', image: 'https://cdn.freepik.com/uploads/2023/01/cute-ganesha-cartoon.png', category: 'Mythology' },
+  { id: 's14', name: 'Hanuman', image: 'https://cdn.freepik.com/uploads/2023/02/cute-hanuman-cartoon.png', category: 'Mythology' },
 ];
